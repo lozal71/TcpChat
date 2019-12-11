e1f110ddce0a868ea8b283bc8e4e22972df7b0e7
Lucky <lucky_like@mail.ru>
2019-12-11 20:00:08 +0300

    first

---
+++ Server/tcpChatServer/tcpChatServer/ClientData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using ServerData;
+
+namespace tcpChatServer
+{
+    class ClientData
+    {
+        public Socket clientSocket;
+        public Thread clientThread;
+        public string id;
+
+        public ClientData()
+        {
+            id = Guid.NewGuid().ToString();
+            clientThread = new Thread(Server.Data_IN);
+            clientThread.Start(clientSocket);
+            sendRegistrationPacketToClient();
+        }
+
+        public ClientData(Socket clientSocket)
+        {
+            this.clientSocket = clientSocket;
+            id = Guid.NewGuid().ToString();
+
+            clientThread = new Thread(Server.Data_IN);
+            clientThread.Start(clientSocket);
+
+            sendRegistrationPacketToClient();
+        }
+
+        public void sendRegistrationPacketToClient()
+        {
+            Packet p = new Packet(PacketType.Registration, "server");
+            p.data.Add(id);
+            clientSocket.Send(p.ToBytes());
+        }
+    }
+}
