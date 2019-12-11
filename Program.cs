6546cb7b3df99b637f6554680022ba1b63579a27
Vyacheslav <slavik.pro.2011@mail.ru>
2019-12-11 20:17:07 +0300

    my first coment

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPchat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
