cf1e99653de467b6b4cc827fecc543f23db7a286
alla <lozal@mail.ru>
2019-12-11 19:53:12 +0300

    1

---
+++ Server/Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using ServerData;
+
+namespace Server
+{
+    class ClientData
+    {
+        public Socket clientSocket;
+        public Thread clientThread;
+        public string id;
+        public ClientData()
+        {
+            id = Guid.NewGuid().ToString();
+            clientThread = new Thread(Server.Data_IN);
+            clientThread.Start(clientSocket);
+            sendRegistrationPacketToClient();
+        }
+
+        public ClientData(Socket clientSocket)
+        {
+            this.clientSocket = clientSocket;
+            id = Guid.NewGuid().ToString();
+            clientThread = new Thread(Server.Data_IN);
+            clientThread.Start(clientSocket);
+            sendRegistrationPacketToClient();
+        }
+        public void sendRegistrationPacketToClient()
+        {
+            Packet p = new Packet(PacketType.Registration, "server");
+            p.data.Add(id);
+            clientSocket.Send(p.ToBytes());
+        }
+    }
+}
